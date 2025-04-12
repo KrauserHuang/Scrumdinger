a07de950a55db6bdcf4b95def2a22b5d29e429fa
Krauser Huang <crescentdence@gmail.com>
2025-04-12 13:46:10 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Tai Chin Huang on 2025/4/12.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
