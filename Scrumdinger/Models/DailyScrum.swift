822c8ed2319a207c7aab55aa0d3643f1e5474f6e
Krauser Huang <crescentdence@gmail.com>
2025-04-12 14:25:57 +0800

    [Feature] Section2: Create a daily scrum model

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Tai Chin Huang on 2025/4/12.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    let title: String
+    let attendees: [String]
+    let lengthInMinutes: Int
+    let theme: Theme
+}
