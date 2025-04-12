4f4fda0a3796ed590e28e81d5f5a6454cbaccde5
Krauser Huang <crescentdence@gmail.com>
2025-04-12 15:35:36 +0800

    [Feature] Section1: Display a list of daily scrums

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Tai Chin Huang on 2025/4/12.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    let scrums: [DailyScrum] = DailyScrum.sampleData
+    ScrumsView(scrums: scrums)
+}
