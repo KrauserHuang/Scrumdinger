580e879a0c079695cf197c0d43bf18e62eb35e88
Andrew Huang <vivatv.it0001@gmail.com>
2025-04-18 09:36:07 +0800

    [Feature] Section1: Display attendees in the edit view
    1. modify DailyScrum property to var(in order to update)

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,32 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by IT-MAC-02 on 2025/4/17.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    
+    @State private var scrum = DailyScrum.emptyScrum
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
