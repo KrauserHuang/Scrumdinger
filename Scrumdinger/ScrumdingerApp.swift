9c589b69e67c885b9a2b57feaa87098ac73c3e27
Andrew Huang <vivatv.it0001@gmail.com>
2025-05-13 14:10:26 +0800

    [Handling errors] Section4: Simulate persistence error
    1. Add another modelContainer modifier to text persistence error(ScrumdingerApp)

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -18,5 +18,8 @@ struct ScrumdingerApp: App {
         // sets the model container in this scene
         // 設定資料儲存容器(資料庫初始化)，來管理SwiftData的資料持久化
         .modelContainer(for: DailyScrum.self)
+        // try! → 強制建立modelContainer
+        // allowsSave: false → 禁止資料持久化(資料只會存在記憶體，不會儲存到磁碟-應用程式關閉後資料就會消失)
+//        .modelContainer(try! .init(for: DailyScrum.self, configurations: .init(allowsSave: false)))
     }
 }
